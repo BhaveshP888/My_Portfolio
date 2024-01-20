dd9039deae4fc4b1c1b219a0a4f1c759e9e25665
Bhavesh Laxman Patil <138896153+BhaveshP888@users.noreply.github.com>
2024-01-20 13:55:47 +0530

    v24

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Navbar Menu
+const nav = document.querySelector('.nav-items');
+const menuBtn = document.querySelector('.menuBtn');
+const menuIcon = document.querySelector('#menuIcon');
+
+menuBtn.addEventListener('click',(e) => {
+        const visbility = nav.getAttribute("data-visible");
+        if (visbility === 'false') {
+                nav.setAttribute("data-visible", "true");
+                menuIcon.classList.add('fa-times');
+                menuIcon.classList.remove('fa-bars');
+        } else if(visbility === "true"){
+                nav.setAttribute("data-visible", "false");
+                menuIcon.classList.add('fa-bars');
+                menuIcon.classList.remove('fa-times');
+        }
+});
+
+
+
+
+
+
+
+//      todo list
 var todoText = document.getElementById("todoText");
 const submitButton = document.querySelector(".Submit");
 const todoContainer = document.getElementById("todoBox")
